59b4c6a3e9aa3a9ca8d78a43ee4054ebe3256209
kark0n <36849198+kark0n@users.noreply.github.com>
2018-03-29 15:06:44 +0300

    оживление модалки через js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var link = document.querySelector(".feedback-btn");
+  
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var overlay = document.querySelector(".modal-overlay");
+
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.add("modal-open");
+overlay.classList.add("modal-open");
+
+});
+
+close.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.remove("modal-open");
+overlay.classList.remove("modal-open");
+});
