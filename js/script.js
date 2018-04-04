fc1d0d9c8b6aa8023e98298e6967f4aa369b0620
kark0n <36849198+kark0n@users.noreply.github.com>
2018-04-04 13:33:44 +0300

    ещё фиксы допок

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
 var overlay = document.querySelector(".modal-overlay");
 
-  link.addEventListener("click", function (evt) {
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-open");
   overlay.classList.add("modal-open");
